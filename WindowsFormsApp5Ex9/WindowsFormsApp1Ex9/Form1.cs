eea51aee8adc950d9dda4950902a14cfd73b8b61
tspatari301721 <84002536+tspatari301721@users.noreply.github.com>
2021-05-15 08:54:12 +0300

    Add files via upload

---
+++ WindowsFormsApp5Ex9/WindowsFormsApp1Ex9/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1Ex9
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random(200);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.FromArgb(r.Next(255), r.Next(255),
+r.Next(255));
+        }
+    }
+}
